4c15bc4b91be2fb2c71b32e0b71813035b70ac52
zhanrui <zhanrui@3dfed3f5-72de-9832-2de3-f9f2af5b567e>
2010-07-01 02:32:53 +0000

    Rejected commit:

---
+++ ccbgwk_v0.1/src/gateway/mbs/xsocketserver/RequestAction.java
@@ -0,0 +1,23 @@
+package gateway.mbs.xsocketserver;
+
+import gateway.mbs.server.domain.RequestData;
+import gateway.mbs.server.protocol.Protocol;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhanrui
+ * Date: 2010-7-1
+ * Time: 8:09:08
+ * To change this template use File | Settings | File Templates.
+ */
+public interface RequestAction {
+    /**
+     * 处理请求，返回反应数据
+     */
+    public void dealReqeust(RequestData requestData,Protocol protocol);
+
+    /**
+     * 是否能处理请求
+     */
+    //public boolean canDeal(RequestData requestData);
+}
---
+++ ccbgwk_v0.1/src/gateway/mbs/xsocketserver/requestaction/T1000Action.java
@@ -0,0 +1,20 @@
+package gateway.mbs.xsocketserver.requestaction;
+
+import gateway.mbs.server.domain.RequestData;
+import gateway.mbs.server.protocol.Protocol;
+import gateway.mbs.xsocketserver.RequestAction;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhanrui
+ * Date: 2010-7-1
+ * Time: 10:27:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class T1000Action implements RequestAction {
+    public void dealReqeust(RequestData requestData, Protocol protocol) {
+        // do nothing
+
+    }
+
+}

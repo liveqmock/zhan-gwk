cb1d059c50ed75fbbe1d97c2f0b37da587e4032e
zhanrui <zhanrui@3dfed3f5-72de-9832-2de3-f9f2af5b567e>
2012-04-27 05:12:13 +0000

    支持多地区（个人信息表新增地区标识字段）

--- ccbgwk_v2.0/webRoot/UI/gwk/cardinfo/cardinfoquery.js
+++ ccbgwk_v2.0/webRoot/UI/gwk/cardinfo/cardinfoquery.js
@@ -12,25 +12,28 @@ function body_resize() {
 function cbRetrieve_Click() {
     var whereStr = "";
     if (trimStr(document.getElementById("cardNo").value) != "") {
-         whereStr += " and ( cbinfo.account like '" + trimStr(document.getElementById("cardNo").value) + "%')";
+         whereStr += " and ( cdinfo.account like '" + trimStr(document.getElementById("cardNo").value) + "%')";
     }
     if (trimStr(document.getElementById("cust_name").value) != "") {
-         whereStr += " and (cbinfo.cardname like'%" + trimStr(document.getElementById("cust_name").value) + "%')";
+         whereStr += " and (cdinfo.cardname like'%" + trimStr(document.getElementById("cust_name").value) + "%')";
     }
-    if (trimStr(document.getElementById("departmentName").value) != "") {
-        whereStr += " and (bgcy.name like '%" + trimStr(document.getElementById("departmentName").value) + "%')";
+    if (trimStr(document.getElementById("deptcode").value) != "") {
+        whereStr += " and (pinfo.deptcode like '%" + trimStr(document.getElementById("deptcode").value) + "%')";
     }
     if (trimStr(document.getElementById("personalID").value) != "") {
-        whereStr += " and (cbinfo.idnumber like '" + trimStr(document.getElementById("personalID").value) + "%')";
+        whereStr += " and (cdinfo.idnumber like '" + trimStr(document.getElementById("personalID").value) + "%')";
     }
     if (trimStr(document.getElementById("sentflag").value) != "") {
-            whereStr += " and cbinfo.sentflag ='" + trimStr(document.getElementById("sentflag").value) + "'";
+            whereStr += " and cdinfo.sentflag ='" + trimStr(document.getElementById("sentflag").value) + "'";
     }
     if (trimStr(document.getElementById("status").value) != "") {
-            whereStr += " and cbinfo.status ='" + trimStr(document.getElementById("status").value) + "'";
+            whereStr += " and cdinfo.status ='" + trimStr(document.getElementById("status").value) + "'";
+    }
+    if (trimStr(document.getElementById("areacode").value) != "") {
+            whereStr += " and areacode ='" + trimStr(document.getElementById("areacode").value) + "'";
     }
 //    alert(whereStr);
-    document.all["cardInfoTab"].whereStr = whereStr + " order by cbinfo.account ";
+    document.all["cardInfoTab"].whereStr = whereStr + " order by areacode, bgcyname, cdinfo.account ";
     document.all["cardInfoTab"].RecordCount = "0";
     document.all["cardInfoTab"].AbsolutePage = "1";
 
